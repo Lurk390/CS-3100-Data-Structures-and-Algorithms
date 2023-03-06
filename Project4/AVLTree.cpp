106ee0c76311e3fbcc5a3ee27e83b1671d38ed88
Lurk390 <elbasiouny.mahmoud@gmail.com>
2023-03-06 00:11:09 -0500

    Finished implementing functions

--- Project4/AVLTree.cpp
+++ Project4/AVLTree.cpp
@@ -1,95 +1,377 @@
 #include "AVLTree.h"
+#include <iostream>
 
 AVLTree::AVLTree()
 {
     root = nullptr;
     size = 0;
-    height = NULL;
 }
 
 AVLTree::~AVLTree()
 {
+    deleteNodes(root);
 }
 
 bool AVLTree::insert(int key, string value)
 {
+    AVLNode *newNode = new AVLNode(key, value, nullptr, nullptr, nullptr, 0);
+
+    // If the tree is empty, make the new node the root
     if (root == nullptr)
     {
-        root = new AVLNode(key, value, nullptr, nullptr, nullptr);
+        root = newNode;
         size++;
-        height = 0;
         return true;
     }
+    // If the tree is not empty, find the correct place to insert the new node
     else
     {
         AVLNode *current = root;
+        AVLNode *parent = nullptr;
         while (current != nullptr)
         {
-            // Check for duplicate key
-            if (key == current->key)
+            parent = current;
+            if (key < current->key)
             {
-                return false;
+                // If the key is less than the current node's key, go left
+                current = current->left;
             }
-            // If key is less than current node, go left
-            else if (key < current->key)
+            else if (key > current->key)
             {
-                // If left child is null, insert new node
-                if (current->left == nullptr)
-                {
-                    current->left = new AVLNode(key, value, current, nullptr, nullptr);
-                    size++;
-                    return true;
-                }
-                // Otherwise, go left
-                else
-                {
-                    current = current->left;
-                }
+                // If the key is greater than the current node's key, go right
+                current = current->right;
             }
-            // If key is greater than current node, go right
             else
             {
-                // If right child is null, insert new node
-                if (current->right == nullptr)
-                {
-                    current->right = new AVLNode(key, value, current, nullptr, nullptr);
-                    size++;
-                    return true;
-                }
-                // Otherwise, go right
-                else
-                {
-                    current = current->right;
-                }
+                // If the key is equal to the current node's key, return false
+                return false;
             }
         }
+
+        if (key < parent->key)
+        {
+            // If the key is less than the parent node's key, insert the new node as the left child
+            parent->left = newNode;
+        }
+        else
+        {
+            // If the key is greater than the parent node's key, insert the new node as the right child
+            parent->right = newNode;
+        }
+        // Set the new node's parent to the parent node
+        newNode->parent = parent;
+        size++;
+        // Update the balance of the new node
+        updateBalance(newNode);
+
+        return true;
+    }
+    return false;
+}
+
+void AVLTree::updateBalance(AVLNode *node)
+{
+    // Check for unbalanced nodes
+    if (node->balance > 1 || node->balance < -1)
+    {
+        // If the node's balance is -2, rotate right
+        if (node->balance > 0)
+        {
+            // If the node's right child's balance is 1, rotate right then left
+            if (node->right->balance < 0)
+            {
+                rightRotate(node->right);
+                leftRotate(node);
+            }
+            // If the node's right child's balance is -1, rotate left
+            else
+            {
+                leftRotate(node);
+            }
+        }
+        // If the node's balance is 2, rotate left
+        else if (node->balance < 0)
+        {
+            // If the node's left child's balance is -1, rotate left then right
+            if (node->left->balance > 0)
+            {
+                leftRotate(node->left);
+                rightRotate(node);
+            }
+            // If the node's left child's balance is 1, rotate right
+            else
+            {
+                rightRotate(node);
+            }
+        }
+        return;
     }
 
-    return false;
+    // If the node's balance is 0, update the balance of the parent node
+    if (node->parent != nullptr)
+    {
+        // If the node is the left child of its parent
+        if (node == node->parent->left)
+        {
+            // Subtract 1 from the parent's balance
+            node->parent->balance -= 1;
+        }
+        // If the node is the right child of its parent
+        else if (node == node->parent->right)
+        {
+            // Add 1 to the parent's balance
+            node->parent->balance += 1;
+        }
+
+        // If the parent's balance is not 0
+        if (node->parent->balance != 0)
+        {
+            // Update the balance of the parent node
+            updateBalance(node->parent);
+        }
+    }
+}
+
+void AVLTree::leftRotate(AVLNode *x)
+{
+    AVLNode *y = x->right;
+    x->right = y->left;
+
+    // Set the parent of the left child of y to x
+    if (y->left != nullptr)
+    {
+        y->left->parent = x;
+    }
+    y->parent = x->parent;
+
+    // If x is the root node, set y as the root node
+    if (x->parent == nullptr)
+    {
+        this->root = y;
+    }
+    // If x is the left child of its parent, set y as the left child of x's parent
+    else if (x == x->parent->left)
+    {
+        x->parent->left = y;
+    }
+    // Otherwise, set y as the right child of x's parent
+    else
+    {
+        x->parent->right = y;
+    }
+    // Set x as the left child of y and set y as the parent of x
+    y->left = x;
+    x->parent = y;
+
+    // Update the balance of x and y
+    x->balance = getBalance(x);
+    y->balance = getBalance(y);
+}
+
+void AVLTree::rightRotate(AVLNode *x)
+{
+    AVLNode *y = x->left;
+    x->left = y->right;
+
+    // Set the parent of the right child of y to x
+    if (y->right != nullptr)
+    {
+        y->right->parent = x;
+    }
+    y->parent = x->parent;
+
+    // If x is the root node, set y as the root node
+    if (x->parent == nullptr)
+    {
+        this->root = y;
+    }
+    // If x is the right child of its parent, set y as the right child of x's parent
+    else if (x == x->parent->right)
+    {
+        x->parent->right = y;
+    }
+    // Otherwise, set y as the left child of x's parent
+    else
+    {
+        x->parent->left = y;
+    }
+    // Set x as the right child of y and set y as the parent of x
+    y->right = x;
+    x->parent = y;
+
+    // Update the balance of x and y
+    x->balance = getBalance(x);
+    y->balance = getBalance(y);
 }
 
 bool AVLTree::find(int key, string &value)
 {
+    // Start at the root node
+    AVLNode *current = root;
+    while (current != nullptr)
+    {
+        // If the key is equal to the current node's key
+        if (key == current->key)
+        {
+            // Set the value to the current node's value and return true
+            value = current->value;
+            return true;
+        }
+        // If the key is less than the current node's key
+        else if (key < current->key)
+        {
+            // Go left
+            current = current->left;
+        }
+        // If the key is greater than the current node's key
+        else
+        {
+            // Go right
+            current = current->right;
+        }
+    }
     return false;
 }
 
-int AVLTree::getHeight()
+bool AVLTree::find(int key) const
 {
+    // Start at the root node
+    AVLNode *current = root;
+    while (current != nullptr)
+    {
+        // If the key is equal to the current node's key
+        if (key == current->key)
+        {
+            // Return true
+            return true;
+        }
+        // If the key is less than the current node's key
+        else if (key < current->key)
+        {
+            // Go left
+            current = current->left;
+        }
+        // If the key is greater than the current node's key
+        else
+        {
+            // Go right
+            current = current->right;
+        }
+    }
+    return false;
+}
+
+vector<string> AVLTree::findRange(int lowkey, int highkey) const
+{
+    vector<string> result;
+    findRangeHelper(root, lowkey, highkey, result);
+    return result;
+}
+
+void AVLTree::findRangeHelper(AVLNode *node, int lowkey, int highkey, vector<string> &result) const
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+
+    if (node->key > highkey)
+    {
+        findRangeHelper(node->left, lowkey, highkey, result);
+    }
+    else if (node->key < lowkey)
+    {
+        findRangeHelper(node->right, lowkey, highkey, result);
+    }
+    else
+    {
+        findRangeHelper(node->left, lowkey, highkey, result);
+        result.push_back(node->value);
+        findRangeHelper(node->right, lowkey, highkey, result);
+    }
+}
+
+void AVLTree::deleteNodes(AVLNode *node)
+{
+    // Check if the node is null
+    if (node == nullptr)
+    {
+        return;
+    }
+    // Post-order traversal to delte all nodes
+    else
+    {
+        deleteNodes(node->left);
+        deleteNodes(node->right);
+        delete node;
+    }
+}
+
+int AVLTree::getHeight() const
+{
+    // Return the height of the root node (entire tree)
+    return getHeight(this->root);
+}
+
+int AVLTree::getHeight(AVLNode *node) const
+{
+    // Check if the node is null
+    if (node == nullptr)
+    {
+        return -1;
+    }
+    else
+    {
+        int leftHeight = getHeight(node->left);
+        int rightHeight = getHeight(node->right);
+
+        // Return the greater of the left and right subtree heights
+        return max(leftHeight, rightHeight) + 1;
+    }
     return 0;
 }
 
-int AVLTree::getSize()
+int AVLTree::getSize() const
 {
-    return 0;
+    return size;
 }
 
-vector<string> AVLTree::findRange(int lowkey, int highkey)
+int AVLTree::getBalance(AVLNode *node) const
 {
-    return vector<string>();
+    // Check if the node is null
+    if (node == nullptr)
+    {
+        return 0;
+    }
+    else
+    {
+        // Return the difference between the left and right subtree heights
+        return getHeight(node->left) - getHeight(node->right);
+    }
+    return 0;
 }
 
 ostream &operator<<(ostream &os, const AVLTree &me)
 {
-    // Right-child-first inorder traversal
+    me.printLargestToSmallest(me.root, 0);
     return os;
+}
+
+void AVLTree::printLargestToSmallest(AVLNode *node, int depth) const
+{
+    // Check if the node is null
+    if (node == NULL)
+        return;
+
+    printLargestToSmallest(node->right, depth + 1);
+
+    // Print the depth of the node as a tab
+    for (int i = 0; i < depth; i++)
+    {
+        cout << "    ";
+    }
+    // Print the node's key and value
+    cout << "(" << (node->key) << ", " << (node->value) << ")" << endl;
+
+    printLargestToSmallest(node->left, depth + 1);
 }
--- Project4/AVLTree.h
+++ Project4/AVLTree.h
@@ -5,39 +5,6 @@ using namespace std;
 
 class AVLTree
 {
-public:
-    // Default constructor. Creates an empty AVL tree.
-    AVLTree();
-
-    // Destructor. Frees all memory allocated to the AVL tree.
-    ~AVLTree();
-
-    // Inserts a new key/value pair into the tree. Duplicate keys are not allowed.
-    // Returns true if the key/value pair is successfully inserted into the map,
-    // and false if the pair could not be inserted. O(log2 n) time complexity.
-    bool insert(int key, string value);
-
-    // If the given key is found in the AVL tree, this function returns true and place
-    // the corresponding value in value. Otherwise this function returns false (and the
-    // value in value can be anything). O(log2 n) time complexity.
-    bool find(int key, string &value);
-
-    // Returns the height of the AVL tree. O(1) time complexity.
-    int getHeight();
-
-    // Returns the total number of nodes (key/value pairs) in the AVL tree.
-    // O(1) time complexity.
-    int getSize();
-
-    // Returns a vector of strings containing all of the values in the tree with keys ≥ lowkey
-    // and ≤ highkey. For each key found in the given range, there will be one value in the
-    // vector. If no matching key/value pairs are found, the function returns an empty vector.
-    vector<string> findRange(int lowkey, int highkey);
-
-    // Prints the tree using the << operator. Overloads the << operator to print the
-    // AVL tree “sideways” using indentation to show the structure of the tree.
-    friend ostream &operator<<(ostream &os, const AVLTree &me);
-
 private:
     // Private inner class for AVLTree nodes
     class AVLNode
@@ -48,18 +15,78 @@ private:
         AVLNode *parent;
         AVLNode *left;
         AVLNode *right;
+        int balance;
 
-        AVLNode(int key, string value, AVLNode *parent, AVLNode *left, AVLNode *right)
+        AVLNode(int key, string value, AVLNode *parent, AVLNode *left, AVLNode *right, int balance = 0)
             : key(key),
               value(value),
               parent(parent),
               left(left),
-              right(right)
+              right(right),
+              balance(balance)
         {
         }
     };
 
     AVLNode *root;
     int size;
-    int height;
+
+public:
+    // Default constructor. Creates an empty AVL tree.
+    AVLTree();
+
+    // Destructor. Frees all memory allocated to the AVL tree.
+    ~AVLTree();
+
+    // Inserts a new key/value pair into the tree. Duplicate keys are not allowed.
+    // Returns true if the key/value pair is successfully inserted into the map,
+    // and false if the pair could not be inserted.
+    bool insert(int key, string value);
+
+    // Checks if node is unbalanced and performs rotations if necessary
+    void updateBalance(AVLNode *node);
+
+    // Single left rotation
+    void leftRotate(AVLNode *node);
+
+    // Single right rotation
+    void rightRotate(AVLNode *node);
+
+    // If the given key is found in the AVL tree, this function returns true and place
+    // the corresponding value in value. Otherwise this function returns false (and the
+    // value in value can be anything).
+    bool find(int key, string &value);
+
+    // If the given key is found in the AVL tree, this function returns true.
+    bool find(int key) const;
+
+    // Returns a vector of strings containing all of the values in the tree with keys ≥ lowkey
+    // and ≤ highkey. For each key found in the given range, there will be one value in the
+    // vector. If no matching key/value pairs are found, the function returns an empty vector.
+    vector<string> findRange(int lowkey, int highkey) const;
+
+    // Helper function for findRange
+    void findRangeHelper(AVLNode *node, int lowkey, int highkey, vector<string> &result) const;
+
+    // Postoreder traversal to delete all nodes
+    void deleteNodes(AVLNode *node);
+
+    // Returns the height of the AVL tree.
+    int getHeight() const;
+
+    // Returns the height of the subtree from a given node.
+    int getHeight(AVLNode *node) const;
+
+    // Returns the total number of nodes (key/value pairs) in the AVL tree.
+    int getSize() const;
+
+    // Returns the balance of a given node.
+    int getBalance(AVLNode *node) const;
+
+    // Prints the tree using the << operator. Overloads the << operator to print the
+    // AVL tree “sideways” using indentation to show the structure of the tree.
+    friend ostream &operator<<(ostream &os, const AVLTree &me);
+
+    // Right-child-first inorder traversal
+    void printLargestToSmallest(AVLNode *node, int depth) const;
 };
