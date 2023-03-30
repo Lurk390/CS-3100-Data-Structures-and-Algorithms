554e6d157e3f4a8577888bd796647bcaa6b31588
Mahmoud Elbasiouny <elbasiouny.mahmoud@gmail.com>
2023-03-30 16:48:58 -0400

    Outlined member functions

---
+++ Project5/Trie.cpp
@@ -0,0 +1,52 @@
+/*
+Project #5 - Alphabet Trie Autocomplete
+
+This is the implementation file for the Trie class. It contains the
+implementation of the member functions of the Trie class.
+
+Author: Mahmoud Elbasiouny
+*/
+#include "Trie.h"
+
+Trie::Trie()
+{
+}
+
+Trie::~Trie()
+{
+}
+
+Trie::Trie(const Trie &)
+{
+}
+
+bool Trie::insert(string word)
+{
+    return false;
+}
+
+int Trie::count()
+{
+    return 0;
+}
+
+int Trie::getSize()
+{
+    return -1;
+}
+
+int Trie::completeCount(string prefix)
+{
+    return 0;
+}
+
+vector<string> Trie::complete(string prefix)
+{
+    vector<string> result;
+    return result;
+}
+
+Trie &Trie::operator=(const Trie &trie)
+{
+    return *this;
+}
---
+++ Project5/Trie.h
@@ -0,0 +1,50 @@
+/*
+Project #5 - Alphabet Trie Autocomplete
+
+This is the header file for the Trie class. It contains the declaration
+of the member functions of the Trie class.
+
+Author: Mahmoud Elbasiouny
+*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Trie
+{
+private:
+public:
+    // Default constructor. Creates an empty Trie.
+    Trie();
+
+    // Destructor. Frees all memory allocated to the Trie.
+    ~Trie();
+
+    // Copy constructor. Creates a new Trie that is a deep copy of the given Trie.
+    Trie(const Trie &);
+
+    // Inserts a new word into the Trie. Duplicate words are not allowed.
+    // Returns true if the word is successfully inserted into the Trie,
+    // and false if the word could not be inserted.
+    bool insert(string);
+
+    // Returns the number of words in the Trie.
+    int count();
+
+    // Returns the total number of nodes in the trie
+    int getSize();
+
+    // Returns the number of words in the dictionary that begin with the given input
+    // string. If no matching words are found, this function will return zero.
+    int completeCount(string);
+
+    // Returns the number of words in the Trie that start with the given input string.
+    // For each word found in the trie, there will be one value in the vector. If no
+    // matching words are found, this function will return an empty vector.
+    vector<string> complete(string);
+
+    // Removes all contents of the original trie and makes an independant copy in
+    // another trie.
+    Trie &operator=(const Trie &);
+};
