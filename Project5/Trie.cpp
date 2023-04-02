e5fe406e2054d1ef0a9c8953188b313a710e6efd
Lurk390 <elbasiouny.mahmoud@gmail.com>
2023-04-02 05:34:06 -0400

    Finished complete and completeCount functions

--- Project5/Trie.cpp
+++ Project5/Trie.cpp
@@ -68,12 +68,53 @@ int Trie::getSize()
 
 int Trie::completeCount(string prefix)
 {
-    return 0;
+    // Return the size of the vector returned by complete()
+    return complete(prefix).size();
 }
 
 vector<string> Trie::complete(string prefix)
 {
     vector<string> result;
+    Node *node = root;
+
+    // Iterate through each character in the prefix
+    for (char ch : prefix)
+    {
+        // Convert the character to an index
+        int index = ch - 'a';
+
+        // If child doesn't exist, return empty vector
+        if (node->children[index] == nullptr)
+        {
+            return result;
+        }
+
+        // Move to the next node
+        node = node->children[index];
+    }
+
+    // If the prefix is a word, add it to the vector
+    if (node->isWord)
+    {
+        result.push_back(prefix);
+    }
+
+    // Iterate through all children of the node
+    for (int i = 0; i < 26; i++)
+    {
+        // If child exists, add it to the vector
+        if (node->children[i] != nullptr)
+        {
+            // Convert the index to a character
+            char ch = i + 'a';
+
+            // Recursively call complete() on the child node
+            vector<string> temp = complete(prefix + ch);
+
+            // Add the results to the vector
+            result.insert(result.end(), temp.begin(), temp.end());
+        }
+    }
     return result;
 }
 
